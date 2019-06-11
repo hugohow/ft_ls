2b803ab4d18c697909f105b4f1ad46edc6cd336a
Hugo How <hugohowchoong@gmail.com>
2019-06-11 22:47:38 +0200

    push

--- srcs/print_file/ft_print_datum.c
+++ srcs/print_file/ft_print_datum.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 02:56:29 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/06/11 22:40:24 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/06/11 22:47:10 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,22 @@
 void	ft_print_datum(t_content *content, t_content *r)
 {
 	struct stat		file_stat;
+	struct passwd	*pwd;
+	struct group	*grp;
 
 	file_stat = content->file_stat;
 	ft_printf(" %*d ", r->len_link, file_stat.st_nlink);
 	if (~content->flag & FLAG_G)
 	{
-		if (content->pw_name)
-			ft_printf("%-*s  ", r->len_owner, content->pw_name);
+		if ((pwd = getpwuid(file_stat.st_uid)) != NULL)
+			ft_printf("%-*s  ", r->len_owner, pwd->pw_name);
 		else
 			ft_printf("%-*d  ", r->len_uid, file_stat.st_uid);
 	}
 	if (~content->flag & FLAG_O)
 	{
-		if (content->gr_name)
-			ft_printf("%-*s", r->len_group, content->gr_name);
+		if ((grp = getgrgid(file_stat.st_gid)) != NULL)
+			ft_printf("%-*s", r->len_group, grp->gr_name);
 		else
 			ft_printf("%-*d", r->len_gid, file_stat.st_gid);
 	}
